5ea824ba5c365081faec2fa1a832b195babdb768
vlatrof <gthbttr@gmail.com>
2022-07-31 03:12:37 +0300

    dev

--- app/src/main/java/com/example/androidsharedpreferencesdemo/MainActivity.kt
+++ app/src/main/java/com/example/androidsharedpreferencesdemo/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.androidsharedpreferencesdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var sharedPreferences: SharedPreferences
+    private val sharedPreferencesChangeListener = initSharedPreferencesListener()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +22,14 @@ class MainActivity : AppCompatActivity() {
         sharedPreferences = getSharedPreferences(App.APP_PREFERENCES, Context.MODE_PRIVATE)
         renderSharedPreferencesData()
         binding.btnSaveData.setOnClickListener { onClickSaveDataButton() }
-        sharedPreferences.registerOnSharedPreferenceChangeListener
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesChangeListener)
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(sharedPreferencesChangeListener)
     }
 
     private fun renderSharedPreferencesData() {
@@ -36,7 +44,31 @@ class MainActivity : AppCompatActivity() {
         sharedPreferences
             .edit()
             .putString(App.PREFERENCES_VALUE, value)
-            .commit()
+            .apply()
+
+    }
+
+    private fun initSharedPreferencesListener(): SharedPreferences.OnSharedPreferenceChangeListener {
+
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener {
+                _, _ ->
+            // it this case we will ignore key because we have only one sharedPreferences key
+            onSharedPreferencesDataChange()
+        }
+
+        return listener
+
+    }
+
+    private fun onSharedPreferencesDataChange() {
+
+        Toast.makeText(
+            this,
+            getString(R.string.toast_message_shared_preferences_data_changed),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        renderSharedPreferencesData()
 
     }
 
